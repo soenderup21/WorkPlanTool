f6a4680cb9e6447db96ac9dde2914d580e778196
soenderup21 <christianstewartsoenderup@hotmail.com>
2018-11-28 13:06:31 +0100

    MyDate
    Added javadocs

--- src/MyDate.java
+++ src/MyDate.java
@@ -6,18 +6,32 @@ public class MyDate
 		month,
 		year;
 	
+	/**
+	 * Constructor Class
+	 * @param day
+	 * @param month
+	 * @param year
+	 */
 	public MyDate(int day, int month, int year) {
 		setDate(day, month, year);
 	}
+	
+	/**
+	 * SetDate sets the day by the use of 3 integers
+	 * @param day
+	 * @param month
+	 * @param year
+	 */
 	public void setDate(int day, int month, int year) {
 		this.day = day;
 		this.month = month;
 		this.year = year;
 	}
-	
-	public MyDate getDate() {
-		return this;
-	}
+	/**
+	 * returns a boolean that returns true if the inputted date is later than the date of the object itself
+	 * @param isLater
+	 * @return returns a boolean indicator
+	 */
 	public boolean isLater(MyDate isLater) {
 		if (this.year < isLater.year)
 		{
@@ -33,10 +47,16 @@ public class MyDate
 		}
 		return false;
 	}
+	/**
+	 * Copies the object
+	 * @return returns the object it's called from
+	 */
 	public MyDate copy() {
 		return this;
 	}
-	
+	/**
+	 * @return returns a string that describes the date in written form
+	 */
 	public String toString() {
 		return day + "/" + month + "/" + year;
 	}
