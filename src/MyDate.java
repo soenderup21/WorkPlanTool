bbc29a62a46b1fda72f3650457ce91798f0633fd
rpetro79 <43003633+rpetro79@users.noreply.github.com>
2018-11-28 13:26:59 +0200

    added getCurrentDate and the 2 classes

--- src/MyDate.java
+++ src/MyDate.java
@@ -1,24 +1,44 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MyDate
 {
-	private int
-		day,
-		month,
-		year;
+	private int day, month, year;
 	
-	public MyDate(int day, int month, int year) {
+	public MyDate(int day, int month, int year) 
+	{
 		setDate(day, month, year);
 	}
-	public void setDate(int day, int month, int year) {
+	public void setDate(int day, int month, int year) 
+	{
 		this.day = day;
 		this.month = month;
 		this.year = year;
 	}
 	
-	public MyDate getDate() {
-		return this;
+	public static MyDate getCurrentDate() 
+	{
+	   int d, m, y;
+      Date date = new Date();
+      
+	   DateFormat dateFormatDay = new SimpleDateFormat("dd");
+      String currentDay = dateFormatDay.format(date);
+      d = Integer.parseInt(currentDay);
+      
+      DateFormat dateFormatMonth = new SimpleDateFormat("MM");
+      currentDay = dateFormatMonth.format(date);
+      m = Integer.parseInt(currentDay);
+      
+      DateFormat dateFormatYear = new SimpleDateFormat("yyyy");
+      currentDay = dateFormatYear.format(date);
+      y = Integer.parseInt(currentDay);
+      
+      MyDate currentDate = new MyDate(d, m, y);
+      return currentDate;
 	}
-	public boolean isLater(MyDate isLater) {
+	public boolean isLater(MyDate isLater) 
+	{
 		if (this.year < isLater.year)
 		{
 			return true;
@@ -33,7 +53,8 @@ public class MyDate
 		}
 		return false;
 	}
-	public MyDate copy() {
+	public MyDate copy() 
+	{
 		return this;
 	}
 	
