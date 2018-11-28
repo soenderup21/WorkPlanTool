18a66762620d34043e4d63c2ad233dc5cbe3d50c
rpetro79 <43003633+rpetro79@users.noreply.github.com>
2018-11-28 15:35:29 +0200

    pleasework

--- src/Analysis.java
+++ src/Analysis.java
@@ -1,5 +1,27 @@
 
 public class Analysis
 {
-
+   private String name;
+   public Analysis(String name)
+   {
+      this.name = name;
+   }
+   public void setName(String name)
+   {
+      this.name = name;
+   }
+   public String getName()
+   {
+      return name;
+   }
+   public String toString()
+   {
+      return name;
+   }
+   public boolean equals(Object obj)
+   {
+      if(!(obj instanceof Analysis))
+         return false;
+      return name.equals(((Analysis)obj).name);
+   }
 }
