7acc24cf8dcdaf0bf52d4fcf3540fb58f040152c
Christian Desktop <christianstewartsoenderup@hotmail.com>
2018-11-29 16:01:15 +0100

    Created Login Screen and initialized it in main method

---
+++ src/LoginScreen.java
@@ -0,0 +1,71 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+import javax.swing.border.TitledBorder;
+
+public class LoginScreen extends JFrame
+{
+	private JPanel containerPanel;
+	private JLabel usernameLabel;
+	private JTextField usernameField;
+	private JLabel passwordLabel;
+	private JPasswordField passwordField;
+	private JButton loginButton;
+    
+	public LoginScreen(String title) {
+		super(title);
+		
+		containerPanel = new JPanel();
+		containerPanel.setLayout(new BoxLayout(containerPanel, BoxLayout.Y_AXIS));
+		
+		usernameLabel = new JLabel("Username: ");
+		passwordLabel = new JLabel("Password: ");
+		
+		usernameField = new JTextField(0);
+		passwordField = new JPasswordField(0);
+		passwordField.setEchoChar('*');
+		
+		loginButton = new JButton("Login");
+		
+		containerPanel.add(usernameLabel);
+		containerPanel.add(usernameField);
+		containerPanel.add(passwordLabel);
+		containerPanel.add(passwordField);
+		containerPanel.add(loginButton);
+		
+		this.add(containerPanel);
+		
+		setSize(200, 150);
+		setVisible(true);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+
+		loginButton.addActionListener(new ActionListener() { 
+			  public void actionPerformed(ActionEvent e) { 
+			    
+				  /*TODO:
+				   * Check password
+				   * Start "Window"
+				   * */
+				  
+			  } 
+			} );
+		
+	}	
+
+}
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main
 {
    public static void main(String[] args)
    {
-      System.out.println("Hello people");
+      LoginScreen LoginFrame = new LoginScreen("WorkPlanTool 0.1.1");
    }
 }
