087ae9cda4dc002451c1c8ad0035cc445803a2b7
Christian Desktop <christianstewartsoenderup@hotmail.com>
2018-11-29 20:32:24 +0100

    Task edit
    Added: Get and set functions, Javadoc.

--- src/Task.java
+++ src/Task.java
@@ -5,23 +5,67 @@ public class Task
 	private Analysis analysis;
 	private MyDate date;
 	
+	/**
+	 * constructor that allows for an empty tast
+	 */
 	public Task() {
 		
 	}
-	
+	/**
+	 * Constructor that initializes a Task
+	 * @param date
+	 * @param analysis
+	 */
 	public Task(MyDate date, Analysis analysis) {
 		this.date = date;
 		this.analysis = analysis;
 	}
+	/**
+	 * add employee to employees
+	 * @param employee
+	 */
 	public void addEmployee(Employee employee) {
 		employees.add(employee);
 	}
+	/**
+	 * adds a multiple of employee to employee
+	 * @param employees
+	 */
 	public void addEmployees(EmployeeList employees) {
 		for (int i = 0; i < employees.getAllEmployees().size(); i++)
 		{
 			this.employees.add(employees.getAllEmployees().get(i));
 		}
 	}
+	/**
+	 * Gets Analysis
+	 * @return
+	 */
+	public Analysis getAnalysis(){
+		return this.analysis;
+	}
+	/**
+	 * sets Analysis
+	 * @param analysis
+	 */
+	public void setAnalysis(Analysis analysis) {
+		this.analysis = analysis;
+	}
+	/**
+	 * gets Date for task
+	 * @return
+	 */
+	public MyDate getDate(){
+		return this.date;
+	}
+	/**
+	 * sets date
+	 * @param date
+	 */
+	public void setDate(MyDate date) {
+		this.date = date;
+	}
+	
 	/*
 	public boolean equals(Task task) {
 		if (this.employees.equals(task.employees))
@@ -36,11 +80,7 @@ public class Task
 		}
 	}
 	*/
-	
 	public String toString() {
-		return date.toString() + "idkidkidkidk";
+		return date.toString(); //only date is displayed 
 	}
-	
-	
-	
 }
