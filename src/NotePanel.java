c798a3657f62d33c0a644e0127fa7ee305b98feb
karlajajic <33891069+karlajajic@users.noreply.github.com>
2018-12-06 12:01:22 +0100

    Note GUI part
    Need to finish it up.
    Add date entering and updating options

--- src/NotePanel.java
+++ src/NotePanel.java
@@ -1,5 +1,127 @@
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 
-public class NotePanel
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+
+public class NotePanel extends JPanel
 {
-
+   private NoteAdapter adapter;
+   private JPanel namePanel;
+   private JLabel nameLabel;
+   private JTextField nameField;
+   
+   private JPanel notePanel;
+   private JLabel noteLabel;
+   private JTextArea noteArea;
+   
+   private JPanel buttonPanel;
+   private JButton createButton;
+   private JCheckBox generalCheck;
+   
+   public NotePanel()
+   {
+      super();
+      adapter=new NoteAdapter("notes.bin");
+      
+      namePanel=new JPanel();
+      nameLabel=new JLabel("Name: ");
+      nameField=new JTextField(15);
+      namePanel.add(nameLabel);
+      namePanel.add(nameField);
+      
+      notePanel=new JPanel();
+      noteLabel=new JLabel("Note: ");
+      noteArea=new JTextArea(12, 25);
+      notePanel.add(noteLabel);
+      notePanel.add(noteArea);
+      
+      Listener listen = new Listener();
+      buttonPanel= new JPanel();
+      generalCheck=new JCheckBox("General");
+      createButton =new JButton("Save");
+      generalCheck.addActionListener(listen);
+      createButton.addActionListener(listen);
+      buttonPanel.add(generalCheck);
+      buttonPanel.add(createButton);
+      
+      setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+      add(namePanel);
+      add(notePanel);
+      add(buttonPanel);
+   }
+   
+   public String getName()
+   {
+      String s="";
+      s=nameField.getText();
+      return s;
+   }
+   
+   public void setName(String name)
+   {
+      nameField.setText(name);
+   }
+   
+   public String getNote()
+   {
+      String s="";
+      s=noteArea.getText();
+      return s;
+   }
+   
+   public void setNote(String note)
+   {
+      nameField.setText(note);
+   }
+   
+   private class Listener implements ActionListener
+   {
+      public void actionPerformed(ActionEvent e)
+      {
+         if(e.getSource()==createButton)
+         {
+            JOptionPane jp=new JOptionPane();
+            if(nameField.getText()=="" || noteArea.getText()=="")
+            {
+               /* JOptionPane.showMessageDialog(null, "One of the fields is empty", "Error", JOptionPane.WARNING_MESSAGE);
+               dispose(); */
+            }
+            Note n=new Note(nameField.getText(), noteArea.getText(), null);
+            if(generalCheck.isSelected()) n.toGeneral();
+            NoteList nl=adapter.getAllNotes();
+            nl.addNote(n);
+            adapter.saveNotes(nl);
+            
+            nameField.setText("");
+            noteArea.setText("");
+            generalCheck.setSelected(false);
+            
+            nl=(NoteList)adapter.getAllNotes();
+            System.out.println(nl);
+         }
+      }
+   }
+   
+ public static void main(String[] args)
+   {
+      NotePanel n=new NotePanel();
+      JFrame frame=new JFrame("Kalendar");
+      Container c = frame.getContentPane();
+      c.add(n);
+      frame.pack();
+      frame.setVisible(true);
+   }
 }
--- src/NoteTab.java
+++ src/NoteTab.java
@@ -1,5 +1,166 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 
-public class NoteTab
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+
+
+public class NoteTab extends JFrame
 {
+   private NoteAdapter adapter;
+   private JPanel mainPanel;
+   
+   private JPanel allNotesPanel;
+   private JTextArea allNotesArea;
+   private JScrollPane allNotesScrollPane;
+   private JComboBox<Note> noteBox;
+   
+   private JButton getButton;
+   private JPanel buttonPanel;
+   private JButton removeButton;
+   
+   private NotePanel noteP;
+   
+   public NoteTab()
+   {
+      super("Notes");
+      adapter= new NoteAdapter("notes.bin");
+      Listener listen = new Listener();
+      
+      allNotesPanel=new JPanel();
+      allNotesArea=new JTextArea(10, 20);
+      allNotesArea.setEditable(false);
+      allNotesScrollPane=new JScrollPane(allNotesArea);
+      allNotesScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+      allNotesPanel.add(allNotesScrollPane);
+      updateAllNotesArea();
+      
+      noteBox=new JComboBox<Note>();
+      noteBox.addActionListener(listen);
+      allNotesPanel.add(noteBox);
+      
+      buttonPanel=new JPanel();
+      getButton=new JButton("Get");
+      getButton.addActionListener(listen);
+      removeButton=new JButton("Remove");
+      removeButton.addActionListener(listen);
+      buttonPanel.add(getButton);
+      buttonPanel.add(removeButton);
+      allNotesPanel.add(buttonPanel);
+     
+      noteP=new NotePanel();
+      
+      mainPanel= new JPanel();
+      mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+      mainPanel.add(allNotesPanel);
+      mainPanel.add(noteP);
+      
+      add(mainPanel);
+      setSize(1000, 650);
+      setVisible(true);
+      setResizable(false);
 
+      setDefaultCloseOperation(EXIT_ON_CLOSE);
+      setLocationRelativeTo(null);
+   }
+   
+   /*private void setNoteBox()
+   {
+      NoteList notes = adapter.getAllNotes();
+      for(int i = 0; i<notes.size(); i++)
+      {
+         noteBox.addItem(notes.getNote(i));
+      }
+   }*/
+   
+   private void updateNoteBox()
+   {
+      int currentIndex = noteBox.getSelectedIndex();
+      
+      noteBox.removeAllItems();
+      
+      NoteList notes = adapter.getAllNotes();
+      for(int i = 0; i<notes.size(); i++)
+      {
+         noteBox.addItem(notes.getNote(i));
+      }
+
+      if(currentIndex==-1 && noteBox.getItemCount()>0)
+      {
+         noteBox.setSelectedIndex(0);
+      }
+      else
+      {
+         noteBox.setSelectedIndex(currentIndex);
+      }
+   }
+   
+   private void updateAllNotesArea()
+   {
+      NoteList n=adapter.getAllNotes();
+      allNotesArea.setText(n.toString());
+      System.out.println(n.toString());
+   }
+   
+   private class Listener implements ActionListener
+   {  
+      public void actionPerformed(ActionEvent e)
+      {
+         if(e.getSource()==noteBox)
+         {
+            if(noteBox.getSelectedItem() instanceof Note)
+            {
+               Note t=(Note)noteBox.getSelectedItem();
+               noteP.setName(t.getName());
+               noteP.setNote(t.getNote());
+            }
+         }
+         
+         if(e.getSource()==noteP.)
+         
+         if(e.getSource()==removeButton)
+         {
+            NoteList n=adapter.getAllNotes();
+            for(int i=0;i<n.size();i++)
+            {
+               if(noteP.getName().equals(n.getNote(i).getName()))
+               {
+                  n.removeNote(i);
+                  noteP.setName("");
+                 adapter.saveNotes(n);
+                 updateAllNotesArea();
+               }
+            }
+         }
+         
+         if(e.getSource()==getButton)
+         {
+            updateAllNotesArea();
+            NoteList n=adapter.getAllNotes();
+            Note[] notes=new Note[n.size()];
+            notes=n.getAllNotes();
+            /*for(int i=0;i<notes.length;i++)
+               if(allNotesArea.getSelectedText()==notes[i].getName())
+               {
+                  nameField.setText(notes[i].getName());
+                  noteArea.setText(notes[i].getNote());
+               }*/
+         }
+      }
+   }
+   
+   public static void main(String[] args)
+   {
+      NoteTab n= new NoteTab();
+   }
+   
 }
